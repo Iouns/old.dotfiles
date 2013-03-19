b84d504f1c5873842c14d8221c7b9605a3ff6e77
Iounes Gardon <iounes.gardon@gmail.com>
2013-03-19 14:01:45 +0100

    Avoid getting user's home permissions modified by rsync.

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,12 @@
 # Simple deployment script
 CMD="$(basename ${0})"
 DOTFILES_PATH="$(dirname ${0})"
-RSYNC_EXCLUDE="${DOTFILES_PATH}/exclude-rsync.txt"
+RSYNC_EXCLUDE="${DOTFILES_PATH}/deploy-exclude.list"
+# Tell Bash to include filenames beginning with a ‘.’ in the results of filename expansion.
+shopt -s dotglob
 
 # Check if there's some modified files.
-CHANGES=$(rsync -an -i --omit-dir-times --exclude-from ${RSYNC_EXCLUDE} ${DOTFILES_PATH}/ ${HOME}/)
+CHANGES=$(rsync -an -i --exclude-from ${RSYNC_EXCLUDE} "${DOTFILES_PATH}"/* "${HOME}"/)
 # If not, get out.
 [[ -z ${CHANGES} ]] && echo 'no pending changes' && exit
 # Print pending changes.
@@ -13,7 +15,7 @@ echo -e "${CHANGES}"
 
 read -p "Deploy dotfiles to => ${HOME}? [y/N]"
 if [[ ${REPLY} =~ ^[Yy]$ ]]; then
-  rsync -a -i --omit-dir-times --exclude-from ${RSYNC_EXCLUDE} ${DOTFILES_PATH}/ ${HOME}/
+  rsync -a -i --exclude-from ${RSYNC_EXCLUDE} "${DOTFILES_PATH}"/* "${HOME}"/
 fi
 
 exit 0
